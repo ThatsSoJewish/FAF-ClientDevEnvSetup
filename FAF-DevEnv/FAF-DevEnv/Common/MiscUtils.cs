f48681aca51ee57690cc196791ff7472cca34411
ThatsSoJerbish <hendyrocks@gmail.com>
2017-10-02 22:11:40 -0500

    Moved towards using pages within th frame:
     - Hoping to eventually integrate other projects
    	- with some user input it should be relatively easy to set up the FA project
     - Created '/Common/MiscUtils.cs' to extract methods that can be used across pages
     - Renamed the client Bash scripts to something different. Better? you tell me
     - Other formatting fixes

---
+++ FAF-DevEnv/FAF-DevEnv/Common/MiscUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FAFDevEnv.Common
+{
+    public static class MiscUtils
+    {
+        /// <summary>
+        /// This displays the minimalistic, built-in version of the windows file browser. 
+        /// </summary>
+        /// <param name="window">Pass in a reference to the Main Window so that the browser will display at its center.</param>
+        /// <returns></returns>
+        public static String ShowFileBrowser(IWin32Window window)
+        {
+            using (FolderBrowserDialog browser = new FolderBrowserDialog())
+            {
+                DialogResult result = browser.ShowDialog(window);
+
+                if (result == DialogResult.OK)
+                {
+                    return browser.SelectedPath;
+                }
+                else { return "CANCEL"; }
+            }
+        }
+    }
+}
